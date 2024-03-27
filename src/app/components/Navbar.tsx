f34f2239a20786596a5a89a10c228276f19984de
sourabh gedar <sourabh.gedar@codezilla.io>
2024-03-27 22:34:37 +0530

    navbar : (added into page)

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,70 @@
+"use client"
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import FitbitIcon from '@mui/icons-material/Fitbit';
+import Container from '@mui/material/Container';
+
+function Navbar() {
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <FitbitIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Blogs
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <FitbitIcon />
+            </IconButton>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Blogs
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import { Typography , Button} from "@mui/material";
+import { Typography, Button } from "@mui/material";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Typography variant="h1">
         Hello world
       </Typography>
