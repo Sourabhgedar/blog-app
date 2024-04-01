74a97ca79ad4bee13ef71e23bd2abd965f764b46
sourabh gedar <sourabh.gedar@codezilla.io>
2024-04-01 18:10:45 +0530

    issue : (navbar manu added in small screem )

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -10,9 +10,12 @@ import Container from '@mui/material/Container';
 import { MAIN_CONSTANTS } from '../constants';
 import { Button } from '@mui/material';
 import { useRouter, usePathname } from 'next/navigation'
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 function Navbar() {
-
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
   const router = useRouter();
   const pathname = usePathname()
 
@@ -26,7 +29,18 @@ function Navbar() {
   const handlePageChange = (page: { name: string, route: string }) => {
     setActivePage(page?.name);
     router.push(`${page.route}`);
+    setAnchorElNav(null)
   };
+
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
 
   return (
     <AppBar position="fixed" sx={{ boxShadow: 'none' }}>
@@ -50,6 +64,44 @@ function Navbar() {
           >
             Blogs
           </Typography>
+
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {MAIN_CONSTANTS.PAGES.map((page, index) => (
+                <MenuItem key={index}  onClick={() => handlePageChange(page)}>
+                  <Typography textAlign="center">{page.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {MAIN_CONSTANTS.PAGES.map((page, index) => (
